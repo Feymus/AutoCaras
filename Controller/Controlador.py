c1fb1d5eecadb4fcacfa296dc811897fd4978cb1
Michael <michaelchoque62@gmail.com>
2017-08-19 10:29:07 -0600

    Agregados trozos: Vectorizar imagen, Matriz de imagenes, Covarianza

--- Controller/Controlador.py
+++ Controller/Controlador.py
@@ -4,6 +4,8 @@ Created on Aug 16, 2017
 @author: HP
 '''
 from Controller.GestorSujeto import GestorSujeto
+import cv2
+import numpy as np
 
 class Controlador(object):
     '''
@@ -13,5 +15,39 @@ class Controlador(object):
     def __init__(self):
         self.listaDeSujetos = GestorSujeto()
     
+    def VectorizarImagen(self, img):
+        
+        arr = cv2.imread(img,0) # Matriz de una imagen en escala de grises
+        arr = cv2.resize(arr, (112, 92)) # Reescalamos la imagen para que sea 112*92
+        flat_arr = arr.ravel() # Vectorizacion de una matriz
+        
+        return flat_arr
     
+    def DefinirMatrizDeImagenes(self, listImgs):
+        
+        contador = 0
+        numeroImagenes = len(listImgs)
+        matrizImgVec = [[]]
+        
+        while (contador+1<=numeroImagenes):
+            
+            imgVectorizada = self.VectorizarImagen(listImgs[contador])
+        
+            if (contador+1 == 1):   #si contador es 1, agrega los elementos como vectores
+                for m in imgVectorizada:
+                    matrizImgVec.append([m])
+            else:   # sino solo los agrega a la columna respectiva
+                y=1
+                for p in imgVectorizada:
+                    matrizImgVec[y].append(p)
+                    y+=1  
+            contador+=1
+        
+        del matrizImgVec[0]
+        
+        return matrizImgVec
+    
+    def DefinirMatrizDeCovarianza(self, matrizImgVec):
+        matrizCov = np.cov(matrizImgVec)
+        return matrizCov
     
