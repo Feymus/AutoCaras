e269394205434f313217ec31e5617aed68b175d1
Nelson <nvgomez11@gmail.com>
2017-08-19 00:01:41 -0600

    matriz con las imagenes vectorizadas listo

--- Controller/main.py
+++ Controller/main.py
@@ -1,64 +1,59 @@
-'''
-Created on Aug 12, 2017
-
-@author: Michael
-'''
-
 import numpy as np
-from PIL import Image
 import os
 import cv2
 from Controller.GestorGeneral import GestorGeneral
 from Controller.GestorSujeto import GestorSujeto
 from Model.Sujeto import Sujeto
+import os  #libreria para contar la cantidad de fotos por carpeta del sujeto
 
 
 # Pruebas con OpenCV, PIL y numpy
 # Consigo la direccion donde se encuentra la imagen
 scriptDir = os.path.dirname(__file__)
-impath = os.path.join(scriptDir, '../Images/unnamed.jpg')
+contador = 0 #contador que recorre imagen por imagen
+list = os.listdir('C:/Users/Nelson/AutoCaras/Images/s1') # devuelve cantidad de fotos en direccion dada
+numeroImagenes = len(list) #numero de imagenes en direccion
+matrizImgVec = [[]]
+
+while (contador+1<=numeroImagenes):
+    impath = os.path.join(scriptDir,'../Images/s1/'+str(contador+1)+'.pgm') #esta direccion es proporcionada por el usuario
+    
+    # Primeras pruebas de vectorizacion
+    arr = cv2.imread(impath,0) # Matriz de una imagen en escala de grises
+    flat_arr = arr.ravel() # Vectorizacion de una matriz
+     
+    if (contador+1 ==1):   #si contador es 1, agrega los elementos como vectores
+        for m in flat_arr:
+            matrizImgVec.append([m])
+    else:   # sino solo los agrega a la columna respectiva
+        y=1
+        for p in flat_arr:
+            matrizImgVec[y].append(p)
+            y+=1  
+    contador+=1
+    
+    
+del matrizImgVec[0] #elimina el primer elemento que era vacio
+print (matrizImgVec)    
+print("muy bien")
+#[131 129 127 ...,  27  27  25]
+#[134 135 136 ...,  29  29  18]
+#[142 143 143 ...,  36  30  26]
+#[145 144 145 ...,  35  30  31]
+#[140 140 143 ...,  40  37  31]
+#[146 145 148 ...,  35  33  30]
+#[141 140 134 ...,  21  18  18]
+#[129 129 129 ...,  17  14  18]
+#[142 144 144 ...,  25  30  29]
+#[121 148 142 ...,  34  29  24]
 
 
-# Matriz de una imagen (la imagen misma)
-image = cv2.imread(impath)
-# Cambio a escala de grises
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#Cambio de tamano de la imagen
-#gray_image = cv2.resize(gray_image, (100, 100))
-
- 
-cv2.imshow("Over the Clouds", image)
-cv2.imshow("Over the Clouds - gray", gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
 
-# Primeras pruebas de vectorizacion
-#arr = cv2.imread(impath,0) # Matriz de una imagen en escala de grises
-#flat_arr = arr.ravel() # Vectorizacion de una matriz
-#print(flat_arr)
-#[240 240 240 ..., 229 229 229]
 
 
-# Vectorizacion segun el documento que paso William
-#arr = np.asarray(gray_image, dtype = np.uint8)
-#print(arr)
-#[[240 240 240 ..., 235 235 235]
-#[240 240 240 ..., 235 235 235]
-#[240 240 240 ..., 235 235 235]
-#..., 
-#[230 230 230 ..., 229 229 229]
-#[230 230 230 ..., 229 229 229]
-#[230 230 230 ..., 229 229 229]]
 
 
-# Pruebas con PIL
-#img = Image.open(impath).convert('RGB')
-#arr = np.array(img)
-#flat_arr = arr.ravel()
-#print(flat_arr)
 
-if __name__ == '__main__':
-    pass
     
