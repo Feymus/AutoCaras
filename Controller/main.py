ad761a1004414895dc928b4d4502d6e38adc7f0c
Michael <michaelchoque62@gmail.com>
2017-08-19 10:29:52 -0600

    Algunas pruebas para unittest

--- Controller/main.py
+++ Controller/main.py
@@ -1,8 +1,5 @@
 import numpy as np
-import os
 import cv2
-from Controller.GestorGeneral import GestorGeneral
-from Controller.GestorSujeto import GestorSujeto
 from Model.Sujeto import Sujeto
 import os  #libreria para contar la cantidad de fotos por carpeta del sujeto
 
@@ -11,18 +8,20 @@ import os  #libreria para contar la cantidad de fotos por carpeta del sujeto
 # Consigo la direccion donde se encuentra la imagen
 scriptDir = os.path.dirname(__file__)
 contador = 0 #contador que recorre imagen por imagen
-list = os.listdir('C:/Users/Nelson/AutoCaras/Images/s1') # devuelve cantidad de fotos en direccion dada
-numeroImagenes = len(list) #numero de imagenes en direccion
+lista = os.listdir('../Images/s1') # devuelve cantidad de fotos en direccion dada
+
+numeroImagenes = len(lista) #numero de imagenes en direccion
 matrizImgVec = [[]]
 
-while (contador+1<=numeroImagenes):
+while (contador+1<=2):
     impath = os.path.join(scriptDir,'../Images/s1/'+str(contador+1)+'.pgm') #esta direccion es proporcionada por el usuario
     
     # Primeras pruebas de vectorizacion
     arr = cv2.imread(impath,0) # Matriz de una imagen en escala de grises
+    arr = cv2.resize(arr, (112, 92)) # Reescalamos la imagen para que sea 112*92
     flat_arr = arr.ravel() # Vectorizacion de una matriz
-     
-    if (contador+1 ==1):   #si contador es 1, agrega los elementos como vectores
+
+    if (contador+1 == 1):   #si contador es 1, agrega los elementos como vectores
         for m in flat_arr:
             matrizImgVec.append([m])
     else:   # sino solo los agrega a la columna respectiva
@@ -34,24 +33,18 @@ while (contador+1<=numeroImagenes):
     
     
 del matrizImgVec[0] #elimina el primer elemento que era vacio
-print (matrizImgVec)    
-print("muy bien")
-#[131 129 127 ...,  27  27  25]
-#[134 135 136 ...,  29  29  18]
-#[142 143 143 ...,  36  30  26]
-#[145 144 145 ...,  35  30  31]
-#[140 140 143 ...,  40  37  31]
-#[146 145 148 ...,  35  33  30]
-#[141 140 134 ...,  21  18  18]
-#[129 129 129 ...,  17  14  18]
-#[142 144 144 ...,  25  30  29]
-#[121 148 142 ...,  34  29  24]
+print (matrizImgVec)
+print("Cantidad de imagenes: " + str(len(matrizImgVec[0])))
+print("Tamano: " + str(len(matrizImgVec)))    
+
+m = np.cov(matrizImgVec)
+print(m)
+print(len(m))
+print(len(m[0]))
 
 
-
-
-
-
+if __name__ == "__main__":
+    pass
 
 
 
