0b90d661f62a591ea9812032839dc71f8781939b
Michael <michaelchoque62@gmail.com>
2017-08-20 11:42:01 -0600

    Agregado funciones de manejo de sujetos

--- Controller/GestorSujeto.py
+++ Controller/GestorSujeto.py
@@ -4,12 +4,28 @@ Created on Aug 16, 2017
 @author: HP
 '''
 from Controller.GestorGeneral import GestorGeneral
+from Model.Sujeto import Sujeto
+
 
 class GestorSujeto(GestorGeneral):
     '''
     classdocs
     '''
 
+
     def __init__(self):
         GestorGeneral.__init__(self)
+        
+    def Agregar(self, dict_sujeto):
+        sujeto_nuevo = Sujeto(dict_sujeto["nombre"])
+        sujeto_nuevo.SetListaFotos(dict_sujeto["fotos"])
+        return GestorGeneral.Agregar(self, sujeto_nuevo)
+    
+    def getAllImgs(self):
+        listaImagenes = []
+        
+        for sujeto in self.listaGeneral:
+            listaImagenes += sujeto.GetListaFotos()
+            
+        return listaImagenes
         
