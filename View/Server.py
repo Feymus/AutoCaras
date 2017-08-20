d1840d7d775b102ac1e224d2acf8bdb2608565ea
Michael <michaelchoque62@gmail.com>
2017-08-20 16:52:17 -0600

    Comentarios

--- View/Server.py
+++ View/Server.py
@@ -2,6 +2,8 @@
 #Created on Aug 20, 2017
 #
 #@author: Michael Choque
+#@author: Nelson Gómez
+#@author: William Espinoza
 
 from flask import Flask
 from flask import request
@@ -9,14 +11,22 @@ from flask import jsonify
 from Controller.Controlador import Controlador
 
 
+## Controlador de la aplicacion
 controlador = Controlador()
+## Servidor de la aplicacion
 app = Flask(__name__, static_url_path='')
 
 
+## Metodo main_index
+#
+# Este metodo manda el script principal de la pagina a la direccion http://127.0.0.1:5000/
 @app.route('/')
 def main_index():
     return app.send_static_file('index.html')
 
+## Metodo submit_imgs_dir
+#
+# Recibe por POST la direccion local de los sujetos a cargar a la aplicacion
 @app.route('/cargaimgs', methods=['GET', 'POST'])
 def submit_imgs_dir():
     if request.method == 'POST':
