6d02e39b2a453e26577d389263b507c3cc96e1d7
WilliamEspinoza <willy3395@gmail.com>
2017-10-07 21:20:45 -0600

    Verificación y aplicación del estándar PEP-8 en controladores

--- Controller/Controlador.py
+++ Controller/Controlador.py
@@ -1,174 +1,164 @@
-##@package docstring
+"""Esta clase gestiona la interaccion entre la pagina web y el modelo
+Gestiona el proceso de entrenamiento y clasificacion de sujetos"""
 #Created on Aug 16, 2017
 #
 #@author: Michael Choque
 #@author: Nelson Gomez
 #@author: William Espinoza
-
+from __future__ import print_function
+import os
+import cv2
 from Controller.GestorSujeto import GestorSujeto
 import numpy as np
-import os, sys, cv2
 
-## Clase controlador
-#
-# Esta clase es la que permite la comunicacion entre los datos de la aplicacion y la interfaz de usuario
+
 class Controlador(object):
-
+    # pylint: disable=too-many-instance-attributes
+    #se considera que los 8 atributos son necesarios
+    """## Clase controlador
+    #
+    # Esta clase es la que permite la comunicacion entre los datos de la aplicacion
+    y la interfaz de usuario"""
     ## Constructor de la clase
-    # 
+    #
     # El constructor unicamente inicializa la lista de Sujetos en la aplicacion
     def __init__(self):
-        self.listaDeSujetos = GestorSujeto()
-    
-    ## Metodo AgregarSujeto
-    #
+        self.lista_de_sujetos = GestorSujeto()
+        self.num_para_entrenar = None
+        self.mean = None
+        self.matriz_img_vec = None
+        self.matriz_de_cov = None
+        self.auto_valores = None
+        self.auto_vectores = None
+        self.pesos = None
+    def agregar_sujeto(self, dict_sujeto):
+        """## Metodo agregar_sujeto
     # @param dict_sujeto un diccionario con el nombre del sujeto y una lista con sus imagenes
-    # @return True/False segun si se agrega con exito el sujeto
-    def AgregarSujeto(self, dict_sujeto):
-        return self.listaDeSujetos.Agregar(dict_sujeto)
-    
-    ## Metodo CargarImagenes
+    # @return True/False segun si se agrega con exito el sujeto"""
+        return self.lista_de_sujetos.agregar(dict_sujeto)
+    def cargar_imagenes(self, img_url, _num_para_entrenar=6):
+        """## Metodo cargar_imagenes
     #
     # @param img_url la direccion local de donde se van a sacar los sujetos y sus imagenes
     # @return una tupla con informacion de si se realizo bien o mal
     # Ejm de una ruta valida:
-    # direccion valida C:/Users/HP/Desktop/TEC/II Semestre 2017/Aseguramiento de calidad/Proyecto/AutoCaras/Images
-    def CargarImagenes(self, img_url, _numParaEntrenar = 6):
-        
-        self.numParaEntrenar = _numParaEntrenar
-
+    #C:/Users/HP/Desktop/TEC/II Semestre 2017/Aseguramiento de calidad/Proyecto/AutoCaras/Images"""
+        self.num_para_entrenar = _num_para_entrenar
         try:
-            # Se saca todos los nombres de carpetas en la ruta dada, estos pasan a ser los nombres de los sujetos
-            sujetos = [sujeto for sujeto in os.listdir(img_url) 
-                            if os.path.isdir(os.path.join(img_url, sujeto))]
-            
-
+            # Se saca todos los nombres de carpetas en la ruta dada, estos pasan a ser
+            #los nombres de los sujetos
+            sujetos = [sujeto for sujeto in os.listdir(img_url)
+                       if os.path.isdir(os.path.join(img_url, sujeto))]
             for sujeto in sujetos:
-
                 #Ahora por cada sujeto se sacan los nombres de las imagenes en su respectiva carpeta
                 imgspath = os.listdir(img_url + '/' + sujeto)
                 dict_sujeto = {}
                 dict_sujeto["nombre"] = sujeto
                 dict_sujeto["fotos"] = []
-                
-                imgspath = imgspath[:_numParaEntrenar]
-                
+                imgspath = imgspath[:_num_para_entrenar]
                 for img in imgspath:
-                    
                     # Por ruta de imagen, la abrimos y transformamos la imagen
                     path = img_url + '/' + sujeto + '/' + img
+                    #pylint: disable=maybe-no-member
+                    #Se agrega esta instruccion debido a que pylint detecta que imread no es
+                    #un miembro de cv2 cuando este si existe
                     arr = cv2.imread(path, 0) # Matriz de una imagen en escala de grises
                     #arr = cv2.resize(arr, (112, 92)) # Reescalamos la imagen para que sea 112*92
                     dict_sujeto["fotos"].append(arr)
-                        
                     # Se guarda el sujeto y sus imagenes en memoria
-                    self.AgregarSujeto(dict_sujeto)
-
-            return (0, "Carga completada!") 
-
-        except FileNotFoundError as FNF:
-            print(FNF)
+                    self.agregar_sujeto(dict_sujeto)
+            return (0, "Carga completada!")
+        except FileNotFoundError as fnf:
+            print (fnf)
             return (-1, "Error: Direccion no encontrada")
-        except:
-            print("Error inesperado: ", sys.exc_info()[0])
+        except Exception as excepcion: # pylint: disable-msg=W0703
+            # pylint: disable-msg=C0301
+            print ("El error '{0}' ha ocurrido. Argumentos {1}.".format(excepcion.message, excepcion.args))
             return (-1, "Error: Desconocido")
-        
-    def Entrenar(self):
-        imgs = self.listaDeSujetos.getAllImgs()
-        self.matrizImgVec, self.mean = self.DefinirMatrizDeImagenes(imgs)
-        self.matrizDeCov = self.DefinirMatrizDeCovarianza(self.matrizImgVec)
-        self.auto_valores, self.auto_vectores = self.DefinirAutoValoresVectores(self.matrizDeCov, self.matrizImgVec)
-        self.pesos = self.DefinirPesos(self.matrizImgVec, self.auto_vectores)
-        #id = self.Clasificar('C:\\Users\\HP\\Desktop\\TEC\\II Semestre 2017\\Aseguramiento de calidad\\Proyecto\\AutoCaras\\Images\\s36\\6.pgm', self.mean, self.auto_vectores, self.pesos)
-        #return self.listaDeSujetos.GetSujetoAt(id)
-
-    ##Funcion que vectoriza una imagen que le entra por parametro
+    def entrenar(self):
+        """Genera una base de conocimiento contra la cual se van a comparar
+        las imagenes a clasificar"""
+        imgs = self.lista_de_sujetos.get_all_imgs()
+        self.matriz_img_vec, self.mean = self.definir_matriz_de_imagenes(imgs)
+        self.matriz_de_cov = self.definir_matriz_de_covarianza(self.matriz_img_vec)
+        # pylint: disable-msg=C0301
+        self.auto_valores, self.auto_vectores = self.definir_auto_valores_vectores(self.matriz_de_cov, self.matriz_img_vec)
+        self.pesos = self.definir_pesos(self.matriz_img_vec, self.auto_vectores)
+        # pylint: disable-msg=C0301
+        #id = self.clasificar('C:\\Users\\HP\\Desktop\\TEC\\II Semestre 2017\\Aseguramiento de calidad\\Proyecto\\AutoCaras\\Images\\s36\\6.pgm',
+        #self.mean, self.auto_vectores, self.pesos)
+        #return self.lista_de_sujetos.get_sujeto_at(id)
+    # pylint: disable=R0201
+    def vectorizar_imagen(self, img):
+        """##Funcion que vectoriza una imagen que le entra por parametro
     #@param img recibe la imagen
-    #@return flat_img devuelve la lista de una imagen vectorizada.
-    def VectorizarImagen(self, img):
+    #@return flat_img devuelve la lista de una imagen vectorizada."""
         flat_img = np.array(img, dtype='float64').flatten() # Vectorizacion de una matriz
         return flat_img
-    
-    ##Funcion que crea la matriz con las imagenes vectorizadas
+    def definir_matriz_de_imagenes(self, list_imgs):
+        """    ##Funcion que crea la matriz con las imagenes vectorizadas
     #@param listImgs la lista de la imagen ya vectorizada
-    #@return MatrizImgVec es la matriz con todas las imagenes vectorizadas de un sujeto
-    def DefinirMatrizDeImagenes(self, listImgs):
-        
-       
-        total = len(listImgs[0]) * len(listImgs[0][0])
-        
-        numeroImagenes = len(listImgs) #numero de imagenes a recorrer
-        matrizImgVec = np.empty(shape=(total, numeroImagenes), dtype='float64')#se crea la matriz que contendra las imagenes vectorizadas
-        
-        
+    #@return MatrizImgVec es la matriz con todas las imagenes vectorizadas de un sujeto"""
+        total = len(list_imgs[0]) * len(list_imgs[0][0])
+        numero_imagenes = len(list_imgs) #numero de imagenes a recorrer
+        matriz_img_vec = np.empty(shape=(total, numero_imagenes), dtype='float64')
+        #se crea la matriz que contendra las imagenes vectorizadas
         img_id = 0
-
-        for training_img in listImgs:
-            
-            imgVectorizada = self.VectorizarImagen(training_img)
-            matrizImgVec[:, img_id] = imgVectorizada[:]
+        for training_img in list_imgs:
+            img_vectorizada = self.vectorizar_imagen(training_img)
+            matriz_img_vec[:, img_id] = img_vectorizada[:]
             img_id += 1
-
-        mean_img = np.sum(matrizImgVec, axis=1) / numeroImagenes
-
-        for j in range(0, numeroImagenes):
-            matrizImgVec[:, j] -= mean_img[:]
-        
-        return (matrizImgVec, mean_img)
-    
-    ## Metodo DefinirMatrizDeCovarianza
+        mean_img = np.sum(matriz_img_vec, axis=1) / numero_imagenes
+        for j in range(0, numero_imagenes):
+            matriz_img_vec[:, j] -= mean_img[:]
+        return (matriz_img_vec, mean_img)
+    def definir_matriz_de_covarianza(self, matriz_img_vec):
+        """    ## Metodo definir_matriz_de_covarianza
     #
-    # @param matrizImgVec el metodo recibe una matriz de imagenes vectorizadas con la que se calculara la matriz de covarianza
-    # @return matrizCov se devuelve la matriz de covarianza calculada 
-    def DefinirMatrizDeCovarianza(self, matrizImgVec):
-        matrizCov = np.matrix(matrizImgVec.transpose()) * np.matrix(matrizImgVec)                             
-        np.divide(matrizCov, len(matrizImgVec[0]), out=matrizCov, casting='unsafe')
-        return matrizCov
-    
-    def DefinirAutoValoresVectores(self, matrizCov, matrizImgVec, _energia = 0.85):
-        
-        auto_valores, auto_vectores = np.linalg.eig(matrizCov)
+    # @param matriz_img_vec el metodo recibe una matriz de imagenes vectorizadas
+    #con la que se calculara la matriz de covarianza
+    # @return matriz_cov se devuelve la matriz de covarianza calculada """
+    #pylint: disable=maybe-no-member
+        matriz_cov = np.matrix(matriz_img_vec.transpose()) * np.matrix(matriz_img_vec)
+        np.divide(matriz_cov, len(matriz_img_vec[0]), out=matriz_cov, casting='unsafe')
+        return matriz_cov
+    def definir_auto_valores_vectores(self, matriz_cov, matriz_img_vec, _energia=0.85):
+        """Recibe como parametros una matriz de covarianza junto a su respectiva matriz
+        de imagenes vectorizadas"""
+        auto_valores, auto_vectores = np.linalg.eig(matriz_cov)
         indices = auto_valores.argsort()[::-1]
         auto_valores = auto_valores[indices]
         auto_vectores = auto_vectores[indices]
-
-        suma_AV = sum(auto_valores[:])
-        contador_AV = 0
-        energia_AV = 0.0
-        
+        suma_autovalores = sum(auto_valores[:])
+        contador_autovalores = 0
+        informacion_en_autovalores = 0.0
         for auto_valor in auto_valores:
-            contador_AV += 1
-            energia_AV += auto_valor / suma_AV
-
-            if energia_AV >= _energia:
+            contador_autovalores += 1
+            informacion_en_autovalores += auto_valor / suma_autovalores
+            if informacion_en_autovalores >= _energia:
                 break
-
-        auto_valores = auto_valores[0:contador_AV]
-        auto_vectores = auto_vectores[0:contador_AV]
-
+        auto_valores = auto_valores[0:contador_autovalores]
+        auto_vectores = auto_vectores[0:contador_autovalores]
         auto_vectores = auto_vectores.transpose()
-        auto_vectores = matrizImgVec * auto_vectores
+        auto_vectores = matriz_img_vec * auto_vectores
         norms = np.linalg.norm(auto_vectores, axis=0)
         auto_vectores = auto_vectores / norms
-
         return (auto_valores, auto_vectores)
-    
-    def DefinirPesos(self, matrizImgVec, auto_vectores):
-        return auto_vectores.transpose() * matrizImgVec
-        
-    def Clasificar(self, img_dir, mean, auto_vectores, pesos):
-        
+    def definir_pesos(self, matriz_img_vec, auto_vectores):
+        """Determina el peso de los autovectores. Recibe como parametros
+        una matriz con imagenes vectorizdas y una matriz con sus respectivos autovectores"""
+        return auto_vectores.transpose() * matriz_img_vec
+    def clasificar(self, img_dir, mean, auto_vectores, pesos):
+        """Recibe como parametros el directorio de la imagen a clasficar, la imagen media
+        de las imagenes de entrenamiento, una matriz de autovectores y los pesos de estos"""
+        #pylint: disable=maybe-no-member
         img = cv2.imread(img_dir, 0)
         img_col = np.array(img, dtype='float64').flatten()
         img_col -= mean
         img_col = np.reshape(img_col, (len(img_col), 1))
-
-        S = auto_vectores.transpose() * img_col
-
-        diff = pesos - S
+        autovectores_transpuesta = auto_vectores.transpose() * img_col
+        diff = pesos - autovectores_transpuesta
         norms = np.linalg.norm(diff, axis=0)
-
         id_cercano = np.argmin(norms)
-        return (id_cercano // self.numParaEntrenar) + 1
-
+        return (id_cercano // self.num_para_entrenar) + 1
     
--- Controller/ControladorPruebaUnitaria.py
+++ Controller/ControladorPruebaUnitaria.py
@@ -1,4 +1,5 @@
-##@package docstring
+"""Este modulo gestiona las distintas pruebas unitarias que se han implementado
+"""
 #Created on Aug 19, 2017
 #
 #@author: Michael Choque
@@ -11,77 +12,86 @@ import numpy as np
 
 ## Clase ControladorTest
 #
-# Esta clase controla lo que tiene que ver con pruebas unitarias de la clase Controlador
 class ControladorTest(unittest.TestCase):
-
+    "# Esta clase controla lo que tiene que ver con pruebas unitarias de la clase Controlador"
     ## Metodo setUp
     #
     # El metodo setUp inicializa todo lo necesario para realizar las pruebas
     def setUp(self):
-        self.foo = Controlador()
-
-    
-    ## Metodo test_CargarImagenes
+        self.tester = Controlador()
+    ## Metodo test_cargar_imagenes
     #
-    # Prueba del metodo CargarImagenes en la clase Controlador, la prueba se realiza asegurando que se consigan todas las imagenes de los sujetos
-    # que esten en la direccion dada y confirmando que la lista de sujetos no este vacia y que existan imagenes (esto ultimo puede no darse)
-    def test_CargarImagenes(self):
-        result = self.foo.CargarImagenes("../Images/")
+    def test_cargar_imagenes(self):
+        """# Prueba del metodo cargar_imagenes en la clase Controlador,
+        #la prueba se realiza asegurando que se consigan todas las imagenes de los sujetos
+        # que esten en la direccion dada y confirmando que la lista de sujetos no este vacia y
+        #que existan imagenes (esto ultimo puede no darse)"""
+        result = self.tester.cargar_imagenes("../Images/")
         self.assertEqual(result[0], 0)
-        self.assertNotEqual(self.foo.listaDeSujetos.getAllImgs(), [])
-        self.assertNotEqual(self.foo.listaDeSujetos.listaGeneral, [])
-
-    ## Metodo test_VectorizarImagen
+        self.assertNotEqual(self.tester.lista_de_sujetos.get_all_imgs(), [])
+        self.assertNotEqual(self.tester.lista_de_sujetos.lista_general, [])
+    ## Metodo test_vectorizar_imagen
     #
-    # Prueba del metodo VectorizarImagen en la clase Controlador, la prueba se realiza asegurando que la imagen se haya aplanado a 1 dimension
-    # y confirmando el tamano final de la imagen
-    def test_VectorizarImagen(self):
-        result = self.foo.VectorizarImagen([[1,2],[3,4]])
-        lista_resultado = np.array([1,2,3,4], dtype='float64')
+    def test_vectorizar_imagen(self):
+        """# Prueba del metodo vectorizar_imagen en la clase Controlador, la prueba
+        se realiza asegurando que la imagen se haya aplanado a 1 dimension
+        # y confirmando el tamano final de la imagen"""
+        result = self.tester.vectorizar_imagen([[1, 2], [3, 4]])
+        lista_resultado = np.array([1, 2, 3, 4], dtype='float64')
         self.assertEqual(result.tolist(), lista_resultado.tolist())
         self.assertEqual(len(result), 4)
-
-    
-    ## Metodo test_MatrizDeImagenes
+    ## Metodo test_matriz_de_imagenes
     #
-    # Prueba del metodo DefinirMatrizDeImagenes en la case Controlador, la prueba se realiza asegurando el resultado de la matriz
-    # segun una lista de "imagenes"
-    def test_MatrizDeImagenes(self):
-        imagenes = [[[1,3]],[[20,40]]]
-        result, mean = self.foo.DefinirMatrizDeImagenes(imagenes)
+    def test_matriz_de_imagenes(self):
+        """# Prueba del metodo definir_matriz_de_imagenes en la case Controlador,
+        la prueba se realiza asegurando el resultado de la matriz
+        # segun una lista de "imagenes"""
+        imagenes = [[[1, 3]], [[20, 40]]]
+        result, mean = self.tester.definir_matriz_de_imagenes(imagenes)
         self.assertEquals(result.tolist(), [[-9.5, 9.5], [-18.5, 18.5]])
         self.assertEquals(mean.tolist(), [10.5, 21.5])
-
-    
-    ## Metodo test_MatrizDeCovarianza
+    ## Metodo test_matriz_de_covarianza
     #
-    # Prueba del metodo DefinirMatrizDeCovarianza en la case Controlador, la prueba se realiza asegurando el resultado de la matriz
-    # segun la cantidad de "imagenes"
-    def test_MatrizDeCovarianza(self):
-        result = self.foo.DefinirMatrizDeCovarianza(np.matrix([[-9.5, 9.5], [-18.5, 18.5]]))
+    def test_matriz_de_covarianza(self):
+        """ # Prueba del metodo definir_matriz_de_covarianza en la case Controlador,
+        #la prueba se realiza asegurando el resultado de la matriz
+        # segun la cantidad de "imagenes"""
+        result = self.tester.definir_matriz_de_covarianza(np.matrix([[-9.5, 9.5], [-18.5, 18.5]]))
         self.assertEquals(result.tolist(), [[432.5, -432.5], [-432.5, 432.5]])
         self.assertEquals(len(result), 2)
-    
-    def test_AutoValoresVectores(self):
+    def test_auto_valores_vectores(self):
+        """Prueba del metodo AutoValoresVectores en la case Controlador,
+        #la prueba se realiza asegurando que los autovalores y autovectores
+        #sean los correctos dada una matriz de imagenes vectorizadas y su respectiva
+        #matriz de covarianza"""
         matriz_cov = np.matrix([[432.5, -432.5], [-432.5, 432.5]])
         matriz_img = np.matrix([[-9.5, 9.5], [-18.5, 18.5]])
-        result_auto_valores, result_auto_vectores = self.foo.DefinirAutoValoresVectores(matriz_cov, matriz_img)
+        # pylint: disable-msg=C0301
+        result_auto_valores, result_auto_vectores = self.tester.definir_auto_valores_vectores(matriz_cov, matriz_img)
         self.assertEquals(result_auto_valores.tolist(), [865.0])
+        # pylint: disable-msg=C0301
         self.assertEquals(result_auto_vectores.tolist(), [[-0.4472135954999579], [-0.8944271909999159]])
 
-    def test_Pesos(self):
+    def test_pesos(self):
+        """Prueba del metodo Pesos en la case Controlador,
+        #la prueba se realiza asegurando que los pesos
+        #sean los correctos dada una matriz de imagenes vectorizadas y sus respectivos
+        #autovectores"""
         matriz_img = np.matrix([[-9.5, 9.5], [-18.5, 18.5]])
         auto_vectores = np.matrix([[-0.4472135954999579], [-0.8944271909999159]])
-        result = self.foo.DefinirPesos(matriz_img, auto_vectores)
+        result = self.tester.definir_pesos(matriz_img, auto_vectores)
         self.assertEquals(result.tolist(), [[20.79543219074804, -20.79543219074804]])
-        
-    def test_Clasificar(self):
-        self.foo.CargarImagenes("../Images/")
-        self.foo.Entrenar()
-        sujeto = self.foo.Clasificar("../Images/s36/6.pgm", self.foo.mean, self.foo.auto_vectores, self.foo.pesos)
-        self.foo.listaDeSujetos.GetSujetoAt(sujeto)
-
-
+    def test_clasificar(self):
+        """Prueba del metodo Clasificar en la case Controlador,
+        #la prueba se realiza asegurando que la clasificacion
+        #sea la correcta dada una imagen cuya clasificion es conocida
+        #autovectores"""
+        self.tester.cargar_imagenes("../Images/")
+        self.tester.entrenar()
+        # pylint: disable-msg=C0301
+        sujeto = self.tester.clasificar("../Images/s36/6.pgm", self.tester.mean, self.tester.auto_vectores, self.tester.pesos)
+        self.tester.lista_de_sujetos.get_sujeto_at(sujeto)
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
